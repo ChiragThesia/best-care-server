4b8aa88a24239e4f0502c2ff2c1fb0dbd3346be9
chiragthesia <harithesia@gmail.com>
2021-03-03 10:28:39 -0600

    hospitals router updated

--- src/routes/hospitals.js
+++ src/routes/hospitals.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const Hospital = require('../models/hospital');
+const axios = require('axios');
 
 router.get('/', (req, res) => {
 	try {
-		console.log('Inside hospital Router');
 		const responseObj = {
 			message: 'Hospital lists can be found at http://localhost:8080/hospital/hospitalList',
 			Source: 'data.cms.gov'
@@ -13,16 +13,13 @@ router.get('/', (req, res) => {
 		res.status(400).json(error.message);
 	}
 });
-
 router.get('/allHospitals/:state', (req, res) => {
 	try {
 		Hospital.find({ state: req.params.state }, function(err, result) {
 			if (err) {
-				console.log(err);
 				res.json(err);
 			}
 			else {
-				console.log('results', result);
 				res.status(200).json(result);
 			}
 		});
@@ -32,3 +29,34 @@ router.get('/allHospitals/:state', (req, res) => {
 });
 
 module.exports = router;
+
+router.get('/list', async (req, res) => {
+	try {
+		const uri =
+			'https://data.cms.gov/provider-data/api/1/datastore/sql?query=[SELECT * FROM 9af2a351-ba4e-5af4-b0e7-542e4bbd1af4][LIMIT 500 OFFSET 3116];&sho_db_columns=true';
+		const api_url = encodeURI(uri);
+		await axios
+			.get(api_url)
+			.then((res) => {
+				const data = res.data;
+				data.forEach((hospital) => {
+					const newHospital = new Hospital({
+						hospitalName: hospital['Facility Name'],
+						state: hospital.State,
+						MRSA_W_Z_Score: hospital['MRSA W Z Score'],
+						paymentReduction: hospital['Payment Reduction']
+					});
+					newHospital.save();
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+		res.status(200).json({
+			message:
+				'new Hospitals were saved, please use the /allHospitals/:state endpoint to get hospital for your state'
+		});
+	} catch (error) {
+		res.status(400).json(error.message);
+	}
+});
